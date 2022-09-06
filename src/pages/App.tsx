0f938698d4897dd7146c868fae8a80fdacae9f3a
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-09-05 22:17:41 -0300

    Módulo 6 vídeo 1 concluído

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,13 +3,22 @@ import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import { ITarefa } from '../types/tarefa';
-import style from './App.module.scss'; 
+import style from './App.module.scss';
+
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelcionada:ITarefa){
+    setSelecionado(tarefaSelcionada);
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas={tarefas} />
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}  
+      />
       <Cronometro />
     </div>
   );
