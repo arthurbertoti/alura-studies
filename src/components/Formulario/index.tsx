a30a6d184a6dd8cf806f90cb83e1d6914cd86e63
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-09-05 22:03:01 -0300

    Módulo 5 concluído

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
-class Formulario extends React.Component {
+import {v4 as uuidvv4} from 'uuid';
+
+class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>}> {
+  state = {
+    tarefa: "",
+    tempo: "00:00:00"
+  }
+  adicionarTarefa(evento: React.FormEvent<HTMLFormElement>){
+    evento.preventDefault();
+    this.props.setTarefas(tarefasAntigas =>
+      [
+        ...tarefasAntigas,
+        {
+          ...this.state,
+          selecionado: false,
+          completado: false,
+          id: uuidvv4()
+        }
+      ]
+    );
+    this.setState({
+      tarefa: "",
+      tempo: "00:00"
+    })
+  }
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -12,6 +37,8 @@ class Formulario extends React.Component {
           <input
             type="text"
             name="tarefa"
+            value={this.state.tarefa}
+            onChange={evento =>{this.setState({...this.state, tarefa:evento.target.value})}}
             id="tarefa"
             placeholder="O que você quer estudar"
             required
@@ -25,13 +52,15 @@ class Formulario extends React.Component {
             type="time"
             step="1"
             name="tempo"
+            value={this.state.tempo}
+            onChange={evento =>{this.setState({...this.state, tempo:evento.target.value})}}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Botao>
+        <Botao type="submit">
           Adiconar
         </Botao>
       </form>
