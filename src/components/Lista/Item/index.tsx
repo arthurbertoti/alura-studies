4334b3884ecd1aab00bdee9fdb8906ff216b04c0
Arthur G. Bertoti <artgbertoti@gmail.com>
2022-09-06 20:26:26 -0300

    Curso concluído

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -15,8 +15,8 @@ export default function item({
 }:Props){
     return(
         <li 
-            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-            onClick={() => selecionaTarefa(
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
+            onClick={() => !completado && selecionaTarefa(
             {
                 tarefa,
                 tempo,
@@ -27,6 +27,7 @@ export default function item({
             )}>
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado && <span className={style.concluido} aria-Label="tarefa completada"></span>}
         </li>
     )
 }
